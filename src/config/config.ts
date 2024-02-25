add2644bd8753dcfcd2b70460d74ed315316f156
unknown <yjs12180825@gmail.com>
2024-02-25 19:58:35 +0900

    config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 
 const envFound = dotenv.config();
-if (envFound.error) {
-  throw new Error("⚠️ Couldn't find .env file");
-}
+// if (envFound.error) {
+//   throw new Error("⚠️ Couldn't find .env file");
+// }
 
 export default {
   PORT: parseInt(process.env.PORT!, 10),
